6e244b870b0b4d44818d72cdb23e0218aca59b1a
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2026-01-20 16:30:43 +0100

    Make catch all ExceptionMapper that ensures exceptions messages are returned in response body

---
+++ facade/src/main/java/dtu/fm22/facade/ExceptionMapper.java
@@ -0,0 +1,17 @@
+package dtu.fm22.facade;
+
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class ExceptionMapper implements jakarta.ws.rs.ext.ExceptionMapper<WebApplicationException> {
+    @Override
+    public Response toResponse(WebApplicationException exception) {
+        return Response.status(exception.getResponse().getStatus())
+                .entity(exception.getMessage())
+                .type(MediaType.TEXT_PLAIN)
+                .build();
+    }
+}
